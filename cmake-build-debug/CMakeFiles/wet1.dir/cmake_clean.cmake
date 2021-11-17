9a2cbf9ec11280d02fe4537fc14ccdbc190d4233
Tom-Guy84 <83006932+Tom-Guy84@users.noreply.github.com>
2021-11-17 18:20:29 +0200

    Upload files given by course staff
    Upload main, CMake, library1,test input and output

---
+++ cmake-build-debug/CMakeFiles/wet1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wet1.dir/main1.cpp.obj"
+  "libwet1.dll.a"
+  "wet1.exe"
+  "wet1.exe.manifest"
+  "wet1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wet1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
