7115aecc445e5fbad08503ab50b70845309d5e78
zivdamir <73081641+zivdamir@users.noreply.github.com>
2021-11-28 22:41:27 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //
-// Created by 0rth0Normal on 19/11/2021.
+// Created by  on 19/11/2021.
 //
 #include <iostream>
 #include "Player.h"
@@ -11,39 +11,58 @@ using std::cout;
 using std::endl;
 int main()
 {
-   Group g1(1);
-   Group g2(1);
-   assert(g1==g2);
-   Group g3(2);
-   assert(!(g3<=g1));
-   AVLTree<int> t;
-    int* b(new int(1));
-   t.insert(*b);
-    int* a(t.find(1));
-   assert(*a==1);
-   t.remove(*b);
-    int* c(t.find(*b));
-   assert(c==nullptr);
-    int* d(new int(1));
-   t.insert(*d);
-    int* e(new int(2));
-   t.insert(*e);
-    int* f(new int(3));
-   t.insert(*f);
-   t.printTree();
-   AVLTree<int> t2;
-   int* aa(new int(9));
-   int* bb(new int (1010));
-   t2.insert(*aa);
-   t2.insert(*bb);
-   t2.printTree();
-   combineTrees(t, t2);
-   t.printTree();
-   delete b;
-   delete d;
-   delete e;
-   delete f;
-   delete aa;
-   delete bb;
-   return 0;
+  AVLTree<int> tree123;
+  int* one=new int(1);
+  int* two=new int(2);
+  int* three=new int(3);
+  AVLTree<int> tree456;
+  int* four=new int(4);
+  int* five=new int(5);
+  int* six=new int(6);
+    tree123.insert(*three);
+    tree123.insert(*two);
+  tree123.insert(*one);
+  tree456.insert(*four);
+  tree456.insert(*five);
+  tree456.insert(*six);
+    try {
+        tree123.remove(*five);
+    }
+    catch(AVLTree<int>::ItemNotExist& a)
+    {
+        cout<<"ok to remove"<<endl;
+    }
+    try {
+        tree123.insert(*one);
+    }
+    catch (AVLTree<int>::ItemExist& b){
+        cout<<"ok to insert"<<endl;
+    }
+    tree456.remove(*four);
+    cout<<" after tree456.remove(*four);"<<endl;
+    tree456.printTree();
+    try {
+        tree456.remove(*four);
+    }
+    catch(AVLTree<int>::ItemNotExist& c)
+    {
+        cout<<"ok to remove after remove"<<endl;
+    }
+    tree456.insert(*four);
+    tree456.printTree();
+    cout<<"ok"<<endl;
+    tree456.remove(*five);
+    cout<<"ok 2"<<endl;
+     cout<<"combi combi"<<endl;
+    combineTrees(tree456,tree123);
+    tree456.printTree();
+    cout<<"combi combi"<<endl;
+    delete one;
+    delete two;
+    delete three;
+    delete four;
+    delete five;
+    delete six;
+
+
 }
